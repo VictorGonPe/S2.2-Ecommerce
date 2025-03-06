760bc676bcf5d5859eae37f7e58bc156e74cbaa3
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-03-06 21:26:23 +0100

    Add Exercise two

--- js/shop.js
+++ js/shop.js
@@ -1,80 +1,7 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-    {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+import {products} from "./products.js";
 
-products = products.map(products => ({
-    ...products,
-    quantity : 0 
-}));
+console.log(products);
 
-// => Reminder, it's extremely important that you debug your code. 
-// ** It will save you a lot of time and frustration!
-// ** You'll understand the code better than with console.log(), and you'll also find errors faster. 
-// ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
-
-// Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
 var count = 0
 var total = 0;
@@ -82,21 +9,30 @@ var total = 0;
 // Exercise 1
 function buy(id) {
     
-    const findProduct = products.find(products => products.id === id);
-   
-    findProduct.quantity === 0 ? cart.push(findProduct) : console.log("Ya esta en el carrito");
+    const productFound = products.find(items => items.id === id);
+
+    const productsInCart = cart.find(items => items.id === id);
+
+    if(!productsInCart){
+        cart.push({...productFound, quantity : 1});
+    }else{
+        productsInCart.quantity++;
+    } 
+
     document.getElementById("count_product").textContent = ++count;
-    findProduct.quantity++
-    
-    console.log(cart);    
+
 }
 
 // Exercise 2
 function cleanCart() {
-    products.map(products => products.quantity = 0);
+
+    cart.forEach(productInCArt => productInCArt.quantity = 0);
     cart = []; 
+    count =  total = 0; 
+
     document.getElementById("count_product").textContent = 0;
-    count = 0;
+    document.getElementById("cart_list").innerHTML = ""; 
+    document.getElementById("total_price").innerHTML = 0; 
 }
 
 // Exercise 3
@@ -125,3 +61,7 @@ function removeFromCart(id) {
 function open_modal() {
     printCart();
 }
+
+window.buy = buy;
+window.cleanCart = cleanCart;
+window.open_modal = open_modal;
